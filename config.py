88ed58a98bc6b3a172ed50a880a56ae08d11cf11
ChenYnag0009 <168895666+ChenYnag0009@users.noreply.github.com>
2025-02-07 00:03:35 +0700

    config.py

--- config.py
+++ config.py
@@ -17,8 +17,8 @@ def is_enabled(value, default):
 
 class Config(object):
     # Bot Information 
-    TECH_VJ_BOT_TOKEN = os.environ.get("TECH_VJ_BOT_TOKEN", "6997371758:AAF2q3HEWlEdR2yLtjfw93l9AXdH8lNdc-I")
-    TECH_VJ_BOT_USERNAME = os.environ.get("TECH_VJ_BOT_USERNAME", "chenyang77_bot") # Bot username without @.
+    TECH_VJ_BOT_TOKEN = os.environ.get("TECH_VJ_BOT_TOKEN", "7766828716:AAETsfHL3qw7pE458coRwOY3R8i3_QxiE5Q")
+    TECH_VJ_BOT_USERNAME = os.environ.get("TECH_VJ_BOT_USERNAME", "longtiandi_url_uploader_bot") # Bot username without @.
     
     # The Telegram API things
     TECH_VJ_API_ID = int(os.environ.get("TECH_VJ_API_ID", "26775695"))
