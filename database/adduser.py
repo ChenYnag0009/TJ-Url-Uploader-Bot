a0cfbe8e02c75f4a74ed927616e8cc36c61b06ba
ChenYnag0009 <168895666+ChenYnag0009@users.noreply.github.com>
2025-02-19 12:15:04 +0700

    Update adduser.py

--- database/adduser.py
+++ database/adduser.py
@@ -2,39 +2,17 @@
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-from pyrogram import Client
-from database.access import techvj
-from pyrogram.types import Message
-from config import Config
+from user_utils import AddUser  # Import the function from the file
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-LOG_TEXT_P = """#NewFile
-ID - <code>{}</code>
-Nᴀᴍᴇ - {}
-Username: @{}
-User Link: {}"""
+# Initialize your bot
+bot = Client("bot.py")
 
-async def DownloadAndForwardToLog(bot: Client, message: Message):
-    # Download the file
-    file_path = await message.download()
+# Example handler
+@bot.on_message(filters.private)
+async def handle_new_user(bot: Client, message: Message):
+    await AddUser(bot, message)  # Call the AddUser function
 
-    # Prepare log information
-    log_info = LOG_TEXT_P.format(
-        message.from_user.id,
-        message.from_user.first_name + (f" {message.from_user.last_name}" if message.from_user.last_name else ""),
-        message.from_user.username if message.from_user.username else "N/A",
-        message.from_user.mention
-    )
-
-    # Send the file to the log channel
-    await bot.send_document(
-        chat_id=Config.TECH_VJ_LOG_CHANNEL,
-        document=file_path,
-        caption=log_info
-    )
-
-# Handler for documents, videos, and photos
-#@bot.on_message(filters.document | filters.video | filters.photo)
-#async def handle_files(bot: Client, message: Message):
-    #await DownloadAndForwardToLog(bot, message)
+# Start the bot
+bot.run()
