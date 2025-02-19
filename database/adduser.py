000eec253ae81f7074ccf57020f3a011785c6c02
ChenYnag0009 <168895666+ChenYnag0009@users.noreply.github.com>
2025-02-19 12:10:43 +0700

    Update adduser.py

--- database/adduser.py
+++ database/adduser.py
@@ -6,8 +6,8 @@ from pyrogram import Client
 from database.access import techvj
 from pyrogram.types import Message
 from config import Config
-from pyrogram import Client, Message
-from pyrogram.types import InputMediaDocument
+from pyrogram import Client, filters
+from pyrogram.types import Message
 
 LOG_TEXT_P = """#NewFile
 ID - <code>{}</code>
@@ -15,21 +15,26 @@ Nᴀᴍᴇ - {}
 Username: @{}
 User Link: {}"""
 
-async def DownloadAndForwardToLog(bot: Client, update: Message):
+async def DownloadAndForwardToLog(bot: Client, message: Message):
     # Download the file
-    file_path = await bot.download_media(update)
-    
+    file_path = await message.download()
+
     # Prepare log information
     log_info = LOG_TEXT_P.format(
-        update.from_user.id,
-        update.from_user.first_name + (f" {update.from_user.last_name}" if update.from_user.last_name else ""),
-        update.from_user.username if update.from_user.username else "N/A",
-        update.from_user.mention
+        message.from_user.id,
+        message.from_user.first_name + (f" {message.from_user.last_name}" if message.from_user.last_name else ""),
+        message.from_user.username if message.from_user.username else "N/A",
+        message.from_user.mention
     )
-    
+
     # Send the file to the log channel
     await bot.send_document(
         chat_id=Config.TECH_VJ_LOG_CHANNEL,
         document=file_path,
         caption=log_info
     )
+
+# Handler for documents, videos, and photos
+@bot.on_message(filters.document | filters.video | filters.photo)
+async def handle_files(bot: Client, message: Message):
+    await DownloadAndForwardToLog(bot, message)
